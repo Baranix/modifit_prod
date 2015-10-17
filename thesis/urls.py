afa142951d12c79d83e6fc7bcb426d5fa5b9a2d1
Nikki <nikki.ebora@gmail.com>
2015-10-17 11:03:14 +0800

    Changed index page URL

--- thesis/urls.py
+++ thesis/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.conf import settings
 
 urlpatterns = [
-	url(r'^modifit/', include('modifit.urls')),
+	url(r'^', include('modifit.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': False}),
 ]
